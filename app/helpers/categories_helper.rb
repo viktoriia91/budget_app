4069b03a7aece44b17ea766b3d7f0bd60dede91d
viktoriia91 <viktoriya.9107@gmail.com>
2024-02-07 15:07:52 +0100

    Modify categories, add seeds

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,13 +1,29 @@
 module CategoriesHelper
   def tailwind_color(category)
-    color_variants = {
-      rose: 'bg-rose-400 hover:bg-rose-400',
-      light_blue: 'bg-sky-400 hover:bg-sky-400',
+    @color_variants = {
+      gray: 'bg-stone-400',
+      rose: 'bg-rose-400',
+      blue: 'bg-sky-600',
+      green: 'bg-green-600',
+      indigo: 'bg-indigo-300',
+      purple: 'bg-purple-500',
+      pink: 'bg-pink-400',
+      red: 'bg-red-500',
+      yellow: 'bg-yellow-300',
+      orange: 'bg-orange-500',
+      teal: 'bg-teal-500',
+      cyan: 'bg-cyan-400',
+      lime: 'bg-lime-400',
+      emerald: 'bg-emerald-400',
     }
-    if category.present? && category.color.present?
-      color_variants[category.color.to_sym]
+    if category == :new_category
+      @color_variants.map { |key, value| [key, key.to_s] }
     else
-      'bg-gray-100 hover:bg-gray-100'
+      if category.present? && category.color.present?
+        @color_variants[category.color.to_sym]
+      else
+        'bg-gray-100 hover:bg-gray-100'
+      end
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
+# Define categories with colors
+categories_with_colors = [
+  { name: 'Other', color: :gray },
+  { name: 'Groceries', color: :orange },
+  { name: 'Transportation', color: :green },
+  { name: 'Utilities', color: :blue },
+  { name: 'Entertainment', color: :rose },
+  { name: 'Clothing', color: :indigo },
+  { name: 'Healthcare', color: :purple },
+  # Add more categories as needed
+]
+
+# Create categories
+categories_with_colors.each do |category_data|
+  category = Category.find_or_create_by(name: category_data[:name])
+  category.update(color: category_data[:color])
+end
+
+# Create expenses for all categories for each month
 (1..12).each do |month|
-  Expense.create(
-    name: "Expense for #{Date::MONTHNAMES[month]}",
-    amount: rand(300),
-    date: Date.new(2023, month, rand(1..28))
-   )
+  categories = Category.all
+  categories.each do |category|
+    Expense.create(
+      name: "Expense for #{Date::MONTHNAMES[month]} - #{category.name}",
+      amount: rand(300),
+      date: Date.new(2023, month, rand(1..28)),
+      category: category
+    )
+  end
 end
